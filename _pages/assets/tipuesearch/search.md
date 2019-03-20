aec54e4319754f5c10c49cc643af3c6bb513582a
viti95 <victornt95@gmail.com>
2019-03-20 17:31:30 +0100

    Optimized search.
    - Removed search filters.
    - Allowed to search 2 letters only.

--- _pages/assets/tipuesearch/search.md
+++ _pages/assets/tipuesearch/search.md
@@ -1,10 +1,12 @@
 ---
-title: Search
-description: "Search this site"
-permalink: /search/
+title: Search cards
+description: "Search cards in this site"
+permalink: /search
 tipue_search_active: true
 exclude_from_search: true
 ---
+
+## Search cards
 
 <link rel="stylesheet" href="{{ "/_pages/assets/tipuesearch/css/normalize.css" | relative_url }}">
 <script src="{{ "/_pages/assets/js/jquery-3.3.1.min.js" | relative_url}}"></script>
@@ -15,7 +17,7 @@ exclude_from_search: true
 
 <form action="{{ page.url | relative_url }}">
   <div class="tipue_search_left"><img src="{{ "/_pages/assets/tipuesearch/search.png" | relative_url }}" class="tipue_search_icon"></div>
-  <div class="tipue_search_right"><input type="text" name="q" id="tipue_search_input" pattern=".{3,}" title="At least 3 characters" required></div>
+  <div class="tipue_search_right"><input type="text" name="q" id="tipue_search_input" pattern=".{2,}" title="At least 2 characters" required></div>
   <div style="clear: both;"></div>
 </form>
 
--- _pages/assets/tipuesearch/tipuesearch_set.js
+++ _pages/assets/tipuesearch/tipuesearch_set.js
@@ -6,50 +6,27 @@ Tipue Search is released under the MIT License
 http://www.tipue.com/search
 */
 
-
-/*
-Stop words
-Stop words list from http://www.ranks.nl/stopwords
-*/
-
-var tipuesearch_stop_words = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
+var tipuesearch_stop_words = [];
 
 
 // Word replace
 
-var tipuesearch_replace = {'words': [
-     {'word': 'tip', 'replace_with': 'tipue'},
-     {'word': 'javscript', 'replace_with': 'javascript'},
-     {'word': 'jqeury', 'replace_with': 'jquery'}
-]};
+var tipuesearch_replace = {'words': []};
 
 
 // Weighting
 
-var tipuesearch_weight = {'weight': [
-     {'url': 'http://www.tipue.com', 'score': 20},
-     {'url': 'http://www.tipue.com/search', 'score': 30},
-     {'url': 'http://www.tipue.com/is', 'score': 10}
-]};
+var tipuesearch_weight = {'weight': []};
 
 
 // Illogical stemming
 
-var tipuesearch_stem = {'words': [
-     {'word': 'e-mail', 'stem': 'email'},
-     {'word': 'javascript', 'stem': 'jquery'},
-     {'word': 'javascript', 'stem': 'js'}
-]};
+var tipuesearch_stem = {'words': []};
 
 
 // Related searches
 
-var tipuesearch_related = {'searches': [
-     {'search': 'tipue', 'related': 'Tipue Search'},
-     {'search': 'tipue', 'before': 'Tipue Search', 'related': 'Getting Started'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'jQuery'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'Blog'}
-]};
+var tipuesearch_related = {'searches': []};
 
 
 // Internal strings
